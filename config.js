fb53b27cd7b1a2e7c9fa8be11689ef1b4a9fe0da
Ayyoub Kerroumi <akerroumi917@gmail.com>
2022-06-04 20:52:45 +0100

    fin

--- config.js
+++ config.js
@@ -2,14 +2,23 @@ const { initializeApp } = require('firebase/app');
 const { getFirestore, collection, getDocs } = require('firebase/firestore');
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyCs5dhbOpG8KSVURRGJo7ZL4zpQIx181LA",
-    authDomain: "testnodejs-e678e.firebaseapp.com",
-    projectId: "testnodejs-e678e",
-    storageBucket: "testnodejs-e678e.appspot.com",
-    messagingSenderId: "1081474329172",
-    appId: "1:1081474329172:web:888ed91a62acde348e1bc1"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCs5dhbOpG8KSVURRGJo7ZL4zpQIx181LA",
+//     authDomain: "testnodejs-e678e.firebaseapp.com",
+//     projectId: "testnodejs-e678e",
+//     storageBucket: "testnodejs-e678e.appspot.com",
+//     messagingSenderId: "1081474329172",
+//     appId: "1:1081474329172:web:888ed91a62acde348e1bc1"
+//   };
+  const firebaseConfig = {
+  apiKey: "AIzaSyB8DY41CC7SOAJuz56Uu8LY8xZCANYTapU",
+  authDomain: "looping-5d781.firebaseapp.com",
+  projectId: "looping-5d781",
+  storageBucket: "looping-5d781.appspot.com",
+  messagingSenderId: "83130354976",
+  appId: "1:83130354976:web:23a405fe005b79c67e1b45",
+  measurementId: "G-GSSX7LNTH8"
+};
 
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
--- controller/game-controller.js
+++ controller/game-controller.js
@@ -2,22 +2,42 @@ const db = require('../config');
 const dba = require('../models');
 const sequelize = require('../models').sequelize;
 const {updateDoc,doc } = require('firebase/firestore');
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
 
 const image = dba.images;
 
 const createGame = async(req, res)=>{
-    const gameId = req.body;
-    // get random image
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+        if (net.family === familyV4Value && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+    const gameId = req.headers["gameid"];
+    console.log(gameId);
+    // // get random image
     const im = await image.findOne({ 
         order: sequelize.random() 
       });
-    console.log(im.dataValues.file);
-    const game = doc(db,"games","new-game-id");
-    await updateDoc(game, {
-        url: im.dataValues.file
-      });
-      
-    res.send(im);
+    try {
+      const game = doc(db,"games",gameId);
+      await updateDoc(game, {
+          image: results["Wi-Fi 2"][0]+":5000/"+im.dataValues.file,
+          hint:im.dataValues.title
+        });
+      }catch(error) {
+        res.status(400).send(error);
+      }
 
 }
 
